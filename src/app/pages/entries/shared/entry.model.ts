6c3417b860ebab2faa26efd2fbed619ddd7c8910
Andre Gustavo Castro <agcastro.py@yahoo.com>
2019-03-05 22:29:10 -0300

    Setting Up EntriesModule

---
+++ src/app/pages/entries/shared/entry.model.ts
@@ -0,0 +1,27 @@
+import {Category} from '../../categories/shared/category.model';
+
+export class Entry {
+
+  constructor(
+    id?: number,
+    name?: string,
+    description?: string,
+    type?: string,
+    amount?: string,
+    date?: string,
+    paid?: boolean,
+    categoryId?: number,
+    category?: Category,
+  ) {}
+
+  get paidText(): string {
+    return this.paid ? 'Pago' : 'Pendente';
+  }
+
+  static types = {
+    expense: 'Despesa',
+    renevue: 'Receita'
+  };
+
+  paid: string;
+}
