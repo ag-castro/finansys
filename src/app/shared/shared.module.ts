745d6a3db42523caa114fed98ccf27a1976e18f2
Andre Gustavo Castro <agcastro.py@yahoo.com>
2019-03-07 21:32:24 -0300

    Added RouterModule to sharedModule and improving page component with breadcrumbComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 
 @NgModule({
   declarations: [BreadcrumbComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
   exports: [
     CommonModule,
+    RouterModule,
     ReactiveFormsModule,
     BreadcrumbComponent
   ]
