dc37b7c7615ea76a3552fcc8d6760f0d04b46159
Andre Gustavo Castro <agcastro.py@yahoo.com>
2019-03-08 00:00:37 -0300

    Added NavBarComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,13 +6,16 @@ import {HttpClientModule} from '@angular/common/http';
 import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {InMemoryDatabase} from '../in-memory-database';
 import {ToastModule} from 'primeng/toast';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
     ToastModule,
@@ -23,6 +26,7 @@ import {ToastModule} from 'primeng/toast';
     HttpClientModule,
     HttpClientInMemoryWebApiModule,
     ToastModule,
+    NavbarComponent
   ]
 })
 export class CoreModule { }
